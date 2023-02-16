16729eb6c6f708884e5e9a028f220c7243b5cddc
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-02-16 10:08:20 -0700

    commit changes so I can change the branch

---
+++ ruby_exercises/mythical-creatures/lib/hobbit.rb
@@ -0,0 +1,27 @@
+class Hobbit
+  attr_reader :name,
+              :disposition,
+              :age
+  def initialize(name, disposition = "homebody")
+    @name = name
+    @disposition = disposition
+    @age = 0
+  end
+
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def adult?
+    return false if @age <= 32
+    true 
+  end
+
+  def old?
+    return true if @age >= 101
+  end
+
+  def has_ring?
+    return true if hobbit.name == "frodo"
+  end
+end
